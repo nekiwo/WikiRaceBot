e7865a40c6ed95742c1533d7ba6db1ca289dc076
nekiwo <geo.panchev@gmail.com>
2022-06-27 18:20:16 -0500

    using gensim isntead of word2vec pip lib

--- RankPages.py
+++ RankPages.py
@@ -1,4 +1,7 @@
-def rank_pages(pages):
-    new_pages = []
+import gensim
+model = gensim.models.Word2Vec.load_word2vec_format('path-to-vectors.txt', binary=False)
 
+def rank_pages(pages):
+    vectors = [model[w] for w in pages]
+    new_pages = []
     return pages
