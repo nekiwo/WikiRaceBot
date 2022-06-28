11f6b594d92faf9afe24899cba7f48f0b407f005
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2022-06-28 16:29:02 -0500

    parse pages into arrays

--- ParsePageTitles.py
+++ ParsePageTitles.py
@@ -1,2 +1,11 @@
 def parse_page_titles(titles):
-    return titles # TODO: return 2d array of words in each title without spaces/commas
+    # input: ["word1, word2", "word3", "word4's word5", "word6 word7 word8"]
+    # output: [["word1", "word2"], ["word3"], ["word4s", "word5"], ["word6", "word7", "word8"]]
+    new_titles = []
+
+    for title in titles:
+        new_title = title.replace("'", "").replace(",", "").replace("-", " ")
+        new_title_arr = new_title.split()
+        new_titles.append(new_title_arr)
+
+    return new_titles
