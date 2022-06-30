78a0146bdd44186d2cb264d5c0becbff5dafd7ba
TheMemedOne <58634064+TheMemedOne@users.noreply.github.com>
2022-06-30 14:22:57 -0500

    monte carlo thing

--- RandomizePages.py
+++ RandomizePages.py
@@ -1,2 +1,21 @@
+import random
+
+
 def randomize_pages(pages):
+    new_pages = []
+
+    i = 0
+    done = False
+    while not done:
+        if pages[i] is not None:
+            r = random.random()
+            if r > 0.2:
+                new_pages.append(pages[i])
+                done = True
+            else:
+                i = i + 1
+        else:
+            new_pages.append(pages[i - 1])
+            done = True
+
     return pages
